e25afa43797e64266b3c269d630652adc09bddb2
Madeline Cameron <code@maddie.today>
2018-06-12 17:01:39 -0400

    refactor(index): Refactor according to eslint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-'use strict'
 
-const { info, warn, error } = require('ara-console')
+
+const { info, warn } = require('ara-console')
 const extend = require('extend')
 const debug = require('debug')('ara:network:node:dht')
 const dns = require('ara-network/dns')
@@ -11,7 +11,7 @@ const dns = require('ara-network/dns')
 const conf = {
   multicast: true,
   loopback: true,
-  ports: [53,5300]
+  ports: [53, 5300]
 }
 
 
@@ -27,45 +27,47 @@ let server = null
  * @param {Object} argv
  * @return Boolean
  */
-async function start(argv) {
-  if (server) {return false}
+async function start() {
+  if (server) { return false }
 
-  info("dns: Starting server")
+  info('dns: Starting server')
 
   server = dns.createServer(conf)
-  server.on('error',onerror)
-  server.on('traffic',ontraffic)
-  server.on('secrets-rotated',onsecretsrotated)
+  server.on('error', onerror)
+  server.on('traffic', ontraffic)
+  server.on('secrets-rotated', onsecretsrotated)
   server.listen(conf.port)
 
   for (const socket of server._sockets) {
     socket.on('listening', () => {
       const { port } = socket.address()
-      info("dns: Listening on port %s", port)
+      info('dns: Listening on port %s', port)
     })
   }
 
   return true
 
   function onerror(err) {
-    if (err && 'EACCES' == err.code) {
+    if (err && err.code == 'EACCES') {
       const { port } = err
       conf.ports.splice(conf.ports.indexOf(port), 1)
-      return debug("error:", err)
-    } else if (true == conf.multicast) {
-      if (err && 'EADDRINUSE' == err.code) {
-        return debug("error:", err)
+      return debug('error:', err)
+    } else if (conf.multicast == true) {
+      if (err && err.code == 'EADDRINUSE') {
+        return debug('error:', err)
       }
     }
+
+    return false
   }
 
   function ontraffic(type, details) {
-    const { message, peer } = details
-    info("dns: traffic: %s: %s: %s", type, message.type, message.opcode)
+    const { message } = details
+    info('dns: traffic: %s: %s: %s', type, message.type, message.opcode)
   }
 
   function onsecretsrotated() {
-    info("dns: Secrets rotated")
+    info('dns: Secrets rotated')
   }
 }
 
@@ -75,9 +77,9 @@ async function start(argv) {
  * @public
  * @return Boolean
  */
-async function stop(argv) {
-  if (null == server) { return false }
-  warn("dns: Stopping server")
+async function stop() {
+  if (server == null) { return false }
+  warn('dns: Stopping server')
   server.destroy(ondestroy)
   return true
   function ondestroy() {
@@ -97,18 +99,18 @@ async function configure(opts, program) {
       .option('port', {
         alias: 'p',
         type: 'number',
-        describe: "Port or ports to listen on",
+        describe: 'Port or ports to listen on',
         default: conf.ports
       })
       .option('loopback', {
         type: 'boolean',
         default: conf.loopback,
-        describe: "Loopback DNS discovery",
+        describe: 'Loopback DNS discovery',
       })
       .option('multicast', {
         type: 'boolean',
         default: conf.multicast,
-        describe: "Multicast DNS discovery",
+        describe: 'Multicast DNS discovery',
       })
 
     if (argv.port) { opts.port = argv.port }
@@ -124,7 +126,7 @@ async function configure(opts, program) {
  * @public
  * @return Object
  */
-async function getInstance(argv) {
+async function getInstance() {
   return server
 }
 
